b5b00ab494a4cac3b5e0d474ecbe952fce5f1fb6
LouisHungLuong <louis.hungluong@setel.com>
2023-12-01 00:13:13 +0700

    Add ids query

--- src/modules/cart/cart.controller.ts
+++ src/modules/cart/cart.controller.ts
@@ -68,8 +68,11 @@ export class CartController {
     description: 'Client get cart v2',
     summary: 'Client get cart v2',
   })
-  clientGetCartV2(@User(UserDataJwtProperties.USERID) userId: string) {
-    return this.cartService.clientGetCartV2(userId);
+  clientGetCartV2(
+    @User(UserDataJwtProperties.USERID) userId: string,
+    @Query() getCartDto: GetSummaryCartDto,
+  ) {
+    return this.cartService.clientGetCartV2(getCartDto, userId);
   }
 
   // @Get('getTbItem')
