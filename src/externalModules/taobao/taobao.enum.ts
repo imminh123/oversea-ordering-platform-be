13ee1f6b13e19ccaea3593a794a25547e9e493c5
LouisHungLuong <louis.hungluong@setel.com>
2023-11-30 16:03:46 +0700

    search taobao item

--- src/externalModules/taobao/taobao.enum.ts
+++ src/externalModules/taobao/taobao.enum.ts
@@ -2,6 +2,7 @@ export enum EndpointEnum {
   GetItemDetail = 'http://api.tmapi.top/taobao/item_detail',
   GetItemDetailV2 = 'https://taobao-advanced.p.rapidapi.com/api',
   SearchItem = 'https://taobao-advanced.p.rapidapi.com/item_search_p',
+  SearchItemV2 = 'https://taobao-api.p.rapidapi.com/api',
 }
 
 export enum SortOption {
@@ -10,4 +11,12 @@ export enum SortOption {
   priceAsc = 'priceAsc',
   priceDesc = 'priceDesc',
   ratingDesc = 'ratingDesc',
+}
+
+export enum SortOptionV2 {
+  default = 'default',
+  salesDesc = 'sales_des',
+  salesAsc = 'sales_asc',
+  priceAsc = 'price_asc',
+  priceDesc = 'price_des',
 }
